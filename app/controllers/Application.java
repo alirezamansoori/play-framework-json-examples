275a5fb62fbd1c4b3e8c417ae307db9739aa82c5
Nathan Armstrong <kenogu.labz@gmail.com>
2014-04-20 01:34:46 -0700

    Add documentation to the primary application classes.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,15 +12,56 @@ import views.html.*;
 import models.PalindromeTestRequest;
 import models.PalindromeTestResult;
 
+/**
+ * The root controller for this simple application.
+ * All server-side logic is represented here.
+ */
 public class Application extends Controller{
+
+	/**
+	 * @return a rendering of the standard Play default homepage
+	 */
 	public static Result index(){
 		return ok(index.render("Your new application is ready."));
 	}
 
+	/**
+	 * @return a rendering of the Palindrome Test page
+	 */
 	public static Result palindromePage(){
 		return ok(palindrome.render());
 	}
 
+	/**
+	 * <p>
+	 *     Tests a string to determine whether it is palindromic, and returns the result of the test.
+	 * </p>
+	 *
+	 * <p>
+	 *     Expects a JSON request of the form:
+	 *     <pre>
+	 * {
+	 *     "testString": "[Test string here]"
+	 * }
+	 *     </pre>
+	 * </p>
+	 *
+	 * <p>
+	 *     A malformed or uninterpretable requests returns a raw string containing an error message.
+	 * </p>
+	 *
+	 * <p>
+	 *     A successful request returns a JSON response of the form:
+	 *     <pre>
+	 * {
+	 *     "isPalindrome": [true|false],
+	 *     "testString": "[Test string here]"
+	 * }
+	 *     </pre>
+	 * </p>
+	 *
+	 * @return a JSON response representing the result of a palindrome test for the requested string.
+	 */
 	@BodyParser.Of(BodyParser.Json.class)
 	public static Result testPalindrome(){
 		JsonNode requestNode = request().body().asJson();
--- app/models/PalindromeTestResult.java
+++ app/models/PalindromeTestResult.java
@@ -4,7 +4,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.libs.Json;
 
+/**
+ * A model class for holding the results of a palindrome test.
+ * Also contains utilities for translating the result to a JSON tree structure.
+ */
 public class PalindromeTestResult{
+
+	/**
+	 * Creates a new palindrome test result representing the given parameters.
+	 * @param isPalindrome Whether the tested string has been evaluated to be a palindrome or not.
+	 * @param testString The string that was tested to produce this result.  Must be non-null.
+	 * @throws java.lang.IllegalArgumentException if testString is null.
+	 */
 	public PalindromeTestResult(boolean isPalindrome, String testString){
 		if(testString == null){
 			throw new IllegalArgumentException("testString must be non-null.");
@@ -14,14 +25,24 @@ public class PalindromeTestResult{
 		this.testString = testString;
 	}
 
+	/**
+	 * @return whether the tested string has been evaluated to be a palindrome or not.
+	 */
 	public boolean isPalindrome(){
 		return isPalindrome;
 	}
 
+	/**
+	 * @return the string that was tested to produce this result.
+	 */
 	public String getTestString(){
 		return testString;
 	}
 
+	/**
+	 * Creates a representation of this object as a JSON node tree.
+	 * @return the root node of a JSON object modeled after this result.
+	 */
 	public JsonNode asJson(){
 		ObjectNode node = Json.newObject();
 		node.put("isPalindrome", isPalindrome);
